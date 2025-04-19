bae7affe8e2d9f979d2e506bb4de6b73f74b38fc
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-04-18 21:46:38 -0700

    Implemented out-of-place array rotation

--- lib/array_algorithms/search.dart
+++ lib/array_algorithms/search.dart
@@ -60,6 +60,17 @@ void reverseInPlace(List<int> arr) {
   }
 }
 
+List<int> rotateArray(List<int> arr) {
+  // use list.filled for better memory performance since we know the
+  // fixed size befoehand
+  List<int> newArray = List.filled(arr.length, 0);
+  for (int i = 1; i < arr.length; i++) {
+    newArray[i - 1] = arr[i];
+  }
+  newArray[newArray.length - 1] = arr[0];
+  return newArray;
+}
+
 void main() {
   List<int> list1 = [1, 3, 5, 7, 9];
   List<int> list2 = [2, 4, 6, 8, 10, 12];
@@ -67,10 +78,14 @@ void main() {
   List<List<int>> tests = [list1, list2, list3];
   List<int> targets = [1, 12, 41];
 
+  // B-search
   // for (int i = 0; i < tests.length; i++) {
   //   print(binarySearch(tests[i], targets[i]));
   // }
-  List<int> toReverse = [2, 4, 6, 8, 10, 12];
-  reverseInPlace(toReverse);
-  print(toReverse);
+  // Reverse
+  // List<int> toReverse = [2, 4, 6, 8, 10, 12];
+  // reverseInPlace(toReverse);
+  // print(toReverse);
+
+  print(rotateArray(list1));
 }
